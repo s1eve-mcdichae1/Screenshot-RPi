34eb69c8133be26a7f07e71ba269509744abf700
s1eve-mcdichae1 <92833764+s1eve-mcdichae1@users.noreply.github.com>
2021-12-11 00:10:52 -0800

    Update screenshot.sh
    Rearrange header.

--- screenshot.sh
+++ screenshot.sh
@@ -11,9 +11,9 @@
  
 rp_module_id="screenshot"
 rp_module_desc="Universal Screenshot with Raspi2PNG"
-rp_module_licence="MIT https://raw.githubusercontent.com/AndrewFromMelbourne/raspi2png/master/LICENSE"
 rp_module_help="To take a screenshot use $datadir/screenshots/screenshot.sh over SSH.\n\nThis script is incompatible with the OpenGL driver."
 rp_module_repo="git https://github.com/AndrewFromMelbourne/raspi2png.git master b3c5599"
+rp_module_licence="MIT https://raw.githubusercontent.com/AndrewFromMelbourne/raspi2png/master/LICENSE"
 rp_module_section="exp"
  
 function depends_screenshot() {
